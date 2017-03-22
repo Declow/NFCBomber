4b33eb27f6b62043ff5e0adc8859dc90710a88fe
Your Name <you@example.com>
2017-03-22 08:54:11 +0100

    Added onclick

--- app/src/main/java/dk/sdu/mmmi/ap/g14/nfcbomber/Home.java
+++ app/src/main/java/dk/sdu/mmmi/ap/g14/nfcbomber/Home.java
@@ -2,6 +2,7 @@ package dk.sdu.mmmi.ap.g14.nfcbomber;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Home extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void onHost(View v) {
+
+    }
+
+    public void onJoin(View v) {
+
     }
 }
