bba3f2116a625a70502b92e80cd3a04072690b9f
Your Name <you@example.com>
2017-03-22 10:53:48 +0100

    Added Communication class

---
+++ app/src/main/java/dk/sdu/mmmi/ap/g14/nfcbomber/Communication.java
@@ -0,0 +1,17 @@
+package dk.sdu.mmmi.ap.g14.nfcbomber;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by declow on 3/22/17.
+ */
+
+public class Communication extends AsyncTask {
+
+    private Exception e;
+
+    @Override
+    protected Object doInBackground(Object[] params) {
+        return null;
+    }
+}
