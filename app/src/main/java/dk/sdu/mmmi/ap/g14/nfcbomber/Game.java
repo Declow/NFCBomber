62a5f457132aaa50472190824a02f17c9d27ec0b
Your Name <you@example.com>
2017-03-22 09:09:40 +0100

    asd

---
+++ app/src/main/java/dk/sdu/mmmi/ap/g14/nfcbomber/Game.java
@@ -0,0 +1,22 @@
+package dk.sdu.mmmi.ap.g14.nfcbomber;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class Game extends AppCompatActivity {
+
+    private static final String TAG = "Game";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        Intent intent = getIntent();
+        String gameType = intent.getStringExtra(Home.GAME_SETUP);
+
+        Log.v(TAG, gameType);
+    }
+}
