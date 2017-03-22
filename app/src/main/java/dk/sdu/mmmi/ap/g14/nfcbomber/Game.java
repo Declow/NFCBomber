92dfb5033828ef1b5c343f35868bb0e252d2fbe7
Steffen Hansen <stefh14@student.sdu.dk>
2017-03-22 10:17:56 +0100

    Added new host activity

--- app/src/main/java/dk/sdu/mmmi/ap/g14/nfcbomber/Game.java
+++ app/src/main/java/dk/sdu/mmmi/ap/g14/nfcbomber/Game.java
@@ -12,7 +12,7 @@ public class Game extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+        setContentView(R.layout.activity_host);
 
         Intent intent = getIntent();
         String gameType = intent.getStringExtra(Home.GAME_SETUP);
